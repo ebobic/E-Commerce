8944c94dd74ba14eb4b72cfc68e6ac19f1ffdc5e
VladZGarcia <113976475+VladZGarcia@users.noreply.github.com>
2025-09-09 15:17:52 +0200

    Move products interface and add category fetch
    Renamed products interface file to lib/interfaces/products.ts and updated imports accordingly. Added fetchProductsByCategory function to retrieve products by category from the API.

--- lib/data/product-data.tsx
+++ lib/data/product-data.tsx
@@ -1,4 +1,4 @@
-import { Products, ProductsResponse } from "./interfaces/products";
+import { Products, ProductsResponse } from "../interfaces/products";
 
 
 export async function fetchProductsData() {
@@ -15,6 +15,12 @@ export async function fetchProductById(id: string) {
 
 export async function fetchSearchProducts(query: string) {
     const response = await fetch(`https://dummyjson.com/products/search?q=${query}`);
+    const { products, total }: ProductsResponse = await response.json();
+    return { products, total };
+}
+
+export async function fetchProductsByCategory(category: string) {
+    const response = await fetch(`https://dummyjson.com/products/category/${category}`);
     const { products, total }: ProductsResponse = await response.json();
     return { products, total };
 }
