aa363c0d6cce4515c10228a15b0eff2602f9ee81
VladZGarcia <113976475+VladZGarcia@users.noreply.github.com>
2025-09-09 12:02:44 +0200

    Refactor product interfaces import and remove logs
    Renamed interfaces.ts to interfaces/products.ts and updated imports in product-data.tsx accordingly. Removed unnecessary console.log statements from product data fetching functions for cleaner code.

--- lib/data/product-data.tsx
+++ lib/data/product-data.tsx
@@ -1,23 +1,20 @@
-import { Products, ProductsResponse } from "./interfaces";
+import { Products, ProductsResponse } from "./interfaces/products";
 
 
 export async function fetchProductsData() {
     const response = await fetch(`https://dummyjson.com/products`);
     const { products, total }: ProductsResponse = await response.json();
-    console.log("Products: ", products);
     return { products, total };
 }
 
 export async function fetchProductById(id: string) {
     const response = await fetch(`https://dummyjson.com/products/${id}`);
     const product: Products = await response.json();
-    console.log("Product by Id: ", product);
     return product;
 }
 
 export async function fetchSearchProducts(query: string) {
     const response = await fetch(`https://dummyjson.com/products/search?q=${query}`);
     const { products, total }: ProductsResponse = await response.json();
-    console.log("Search Products: ", products);
     return { products, total };
 }
