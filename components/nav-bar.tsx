15c96652b1f7f178144085db50e24257d16525df
Consolider <christian.spector@protonmail.com>
2025-09-09 11:27:59 +0200

    Added icons to Navbar

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import Image from "next/image";
 import data from "@/lib/data/pages.json";
 
 export default function NavBar() {
@@ -8,6 +9,7 @@ export default function NavBar() {
             <h2 className="text-3xl font-bold text-blue-900">MegaMart</h2>
             </Link>
 
+            <div className="flex gap-4 items-center justify-between">
             <ul className="flex">
                 {data["pages"].map((link, index) => (
                 <li key={index}>
@@ -20,6 +22,31 @@ export default function NavBar() {
                 </li>
                 ))}
             </ul>
+            <Link href={"#"}>
+                <Image
+                    src="/search.svg"
+                    alt="Search Icon"
+                    width={15}
+                    height={15}
+                />
+            </Link>
+            <Link href={"#"}>
+                <Image
+                    src="/user.svg"
+                    alt="User Icon"
+                    width={15}
+                    height={15}
+                />
+            </Link>
+            <Link href={"#"}>
+                <Image
+                    src="/handbag.svg"
+                    alt="Handbag Icon"
+                    width={15}
+                    height={15}
+                />
+            </Link>
+            </div>
         </nav>
     )
 }
