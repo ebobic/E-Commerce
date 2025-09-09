0ef18887aa21d247f0d6bd580baafb9ab3cc9d22
VladZGarcia <113976475+VladZGarcia@users.noreply.github.com>
2025-09-09 15:19:04 +0200

    Remove unused product data fetch in Home page
    Eliminated the fetchProductsData call and related console log from the Home page component as they were not being used.

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,4 @@ import Image from "next/image";
 import { fetchProductById, fetchProductsData, fetchSearchProducts } from "../lib/data/product-data";
 
 export default async function Home() {
-  const data = await fetchProductsData();
-  console.log("total", data.total);
 }
