8a01ab1d384f32d29b92121f4e7f7861c00c20d6
ebobic <ebobic95@gmail.com>
2025-09-11 11:03:58 +0200

    resolved merge conflict

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 
 import Hero from "@/components/hero";
 import BenefitsBanner from "@/components/BenefitsBanner";
+import ContactForm from "@/components/contact-form";
+import AboutUs from "@/components/AboutUs";
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <BenefitsBanner/>
+      <AboutUs/>
+      <ContactForm />
     </main>
     );
 }
