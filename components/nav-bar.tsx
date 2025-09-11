52db9ccb65effbe142ff5c30e5a07ef2e348bfd8
Linda <lindakowalik@gmail.com>
2025-09-11 11:35:19 +0200

    made hamburger menu interactive with useState

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -1,8 +1,13 @@
+"use client"
+
 import Link from "next/link"
 import Image from "next/image";
 import data from "@/lib/data/pages.json";
+import { useState } from "react";
 
 export default function NavBar() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
         <nav className="breakout flex py-8 items-center justify-between bg-white">
             <Link href={"/"}>
@@ -10,22 +15,24 @@ export default function NavBar() {
             </Link>
 
             <div className="flex gap-8 items-center justify-between">
-                <ul className="flex flex-col absolute top-20 left-0 w-full px-4 bg-white font-semibold text-neutral-800 md:flex-row md:gap-8 md:static md:top-auto md:left-auto md:font-normal">
-                    {data["pages"].map((link, index) => (
-                    <li key={index}>
-                        <Link
-                            className="flex items-center justify-between py-2 hover:text-blue-900"
-                            href={link.href}
-                        >
-                            {link.label}
-                            <svg xmlns="http://www.w3.org/2000/svg" height="16px" viewBox="0 -960 960 960" width="16px" fill="currentColor" className="w-3 h-3 md:hidden">
-                            <path d="m321-80-71-71 329-329-329-329 71-71 400 400L321-80Z"/>
-                        </svg>
-                        </Link>
-                        
-                    </li>
-                    ))}
-                </ul>
+                {menuOpen && (
+                    <ul className="flex flex-col absolute top-20 left-0 w-full px-4 bg-white font-semibold text-neutral-800 md:flex-row md:gap-8 md:static md:top-auto md:left-auto md:font-normal">
+                        {data["pages"].map((link, index) => (
+                        <li key={index}>
+                            <Link
+                                className="flex items-center justify-between py-2 hover:text-blue-900"
+                                href={link.href}
+                            >
+                                {link.label}
+                                <svg xmlns="http://www.w3.org/2000/svg" height="16px" viewBox="0 -960 960 960" width="16px" fill="currentColor" className="w-3 h-3 md:hidden">
+                                <path d="m321-80-71-71 329-329-329-329 71-71 400 400L321-80Z"/>
+                            </svg>
+                            </Link>
+                            
+                        </li>
+                        ))}
+                    </ul>
+                )}
                 
                 <div className="flex gap-4">
                     <Link href={"#"}>
@@ -58,16 +65,19 @@ export default function NavBar() {
                         />
                         </div>
                     </Link>
-                    <Link href={"#"}>
-                        <div className="w-[15px] h-[15px] relative md:hidden">
+                    
+                    <button 
+                        className="w-[15px] h-[15px] relative md:hidden"
+                        onClick={() => setMenuOpen(!menuOpen)}
+                    >
                         <Image
                             src="/menu.svg"
                             alt="Menu icon"
                             layout="fill"
                             objectFit="contain"
                         />
-                        </div>
-                    </Link>
+                    </button>
+
                 </div>
             </div>
         </nav>
