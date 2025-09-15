faf815c635cd0928357c1d415ca0f72925705c11
Linda <lindakowalik@gmail.com>
2025-09-15 15:25:47 +0200

    extracted code for mobile navigation into its own component: MobileMenu

--- components/Nav/NavBar.tsx
+++ components/Nav/NavBar.tsx
@@ -6,6 +6,7 @@ import data from "@/lib/data/pages.json";
 import { useState } from "react";
 import SearchBar from "../search-bar";
 import RenderLink from "./render-link";
+import MobileMenu from "./MobileMenu";
 
 
 export default function NavBar() {
@@ -15,6 +16,7 @@ export default function NavBar() {
     return (
         <div className="relative">
         <nav className="flex items-center justify-between py-8 bg-white relative z-50 px-6">
+            
             {/* ul/li nav */}
             <div className="hidden md:flex text-1xl font-semibold md:justify-start md:w-1/3">
 
@@ -27,21 +29,9 @@ export default function NavBar() {
                     ))}
                 </ul>
             </div>
-                {/* mobile nav*/}
-                {menuOpen && (
-                  <div className="md:flex text-1xl font-semibold md:justify-start md:w-1/3">
-                    <ul className="flex flex-col absolute top-20 left-0 w-full px-4 bg-white text-neutral-800 md:flex-row md:gap-8 md:static md:top-auto md:left-auto md:font-normal">
-                        {data["pages"].map((link, index) => (
-                        <li key={index} className="flex items-center justify-between py-2 text-neutral-800 hover:text-blue-900">
-                            <RenderLink {...link} />
-                            <svg xmlns="http://www.w3.org/2000/svg" height="16px" viewBox="0 -960 960 960" width="16px" fill="currentColor" className="w-3 h-3 md:hidden">
-                                <path d="m321-80-71-71 329-329-329-329 71-71 400 400L321-80Z"/>
-                            </svg>
-                        </li>
-                        ))}
-                    </ul>
-                    </div>
-                )}
+
+            {/* mobile nav*/}
+            {menuOpen && <MobileMenu/>}
             
             {/* logo */}
             <div className="flex w-1/2 md:w-1/3 md:justify-around">
