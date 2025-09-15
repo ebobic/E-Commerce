3f8f67a7ab53c09a474d7f0d39ab87481fb8f05b
VladZGarcia <113976475+VladZGarcia@users.noreply.github.com>
2025-09-15 14:03:56 +0200

    Add product details page and link from product card
    Implemented dynamic product details page that fetches product data by ID and displays it using the ProductCard component. Updated ProductCard to link to the product details page for each product.

--- app/pages/products/[id]/page.tsx
+++ app/pages/products/[id]/page.tsx
@@ -1,5 +1,37 @@
-export default function Product() {
+import ProductCard from "@/components/product-card";
+import { fetchProductById } from "@/lib/data/product-data";
+
+
+async function ProductDetails(
+    {
+       params    
+    }: {
+        params: Promise<{ id: string }>;
+}) {
+    const productId = await params;
+    console.log("productId: ", productId)
+    //fetch Product Data
+    const productData = await fetchProductById(productId.id)
     return (
-        <div></div>
+        <div>
+            {
+                <ProductCard {...productData} />
+                
+            }
+        </div>
     )
 }
+
+export default async function ProductById(
+    {
+        params
+        
+    }: {
+            params: Promise<{ id: string }>;
+        }) {
+    return (
+        <main>
+            <ProductDetails params={params} />
+        </main>
+    )
+    }
--- components/product-card.tsx
+++ components/product-card.tsx
@@ -1,6 +1,7 @@
 import { fetchProductsData } from "@/lib/data/product-data";
 import { Product, ProductsResponse } from "@/lib/interfaces/products";
 import Image from "next/image";
+import Link from "next/link";
 
 /* async function getProductDetails() {
     const products = await fetchProductsData();
@@ -13,13 +14,15 @@ export default async function ProductCard(product: Product) {
   const imageUrl = product.images[0];
 
   return (
+    <Link href={`/pages/products/${product.id}`}
+    className="">
     <article
       className={`relative w-[210px] h-[350px] flex flex-col bg-gradient-to-br from-white to-blue-100 border-yellow-700 rounded-4xl font-verdana 
         transform-gpu transition-all duration-200 ease-out hover:[transform:translateY(-1px)_scale(1.03)_rotate(0deg)]
         shadow-[0_10px_20px_rgba(0,0,0,0.3)] hover:shadow-[0_10px_20px_rgba(0,0,0,0.3)]
         before:absolute before:inset-0 before:rounded-xl before:bg-black/5 before:opacity-0 hover:before:opacity-100 before:transition-opacity before:pointer-events-none
         after:absolute after:inset-0 after:rounded-xl after:bg-gradient-to-br after:from-white/10 after:to-transparent after:pointer-events-none`}
-    >
+        >
       {/* Product Image */}
       <div className="relative w-full h-1/2 scale-100 flex items-center justify-center mx-auto p-8 pb-2 -translate-y-4 transition-transform duration-300 group-hover:-translate-y-6 will-change-transform">
         <Image
@@ -29,7 +32,7 @@ export default async function ProductCard(product: Product) {
           alt=""
           className="w-full h-full object-cover scale-115 object-center drop-shadow-xl transition-all duration-300 group-hover:scale-150 group-hover:drop-shadow-2xl will-change-transform"
           priority
-        />
+          />
       </div>
       {/* Product Details */}
       <div className="w-full h-1/3 rounded-b-4xl flex-1 bg-white p-4">
@@ -50,11 +53,12 @@ export default async function ProductCard(product: Product) {
           <button
             type="button"
             className="flex items-center justify-center px-3 py-2 z-50 text-white bg-black rounded-4xl text-lg font-extralight cursor-pointer"
-          >
+            >
             <span className="text-xs">Buy</span>
           </button>
         </div>
       </div>
     </article>
+            </Link>
   );
 }
