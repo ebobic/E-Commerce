acba39c6667e52c263e99eaa521dda11f29be879
Consolider <christian.spector@protonmail.com>
2025-09-16 15:32:57 +0200

    refactor components

--- app/pages/products/page.tsx
+++ app/pages/products/page.tsx
@@ -1,3 +1,7 @@
+
+import { fetchAllCategories } from "@/lib/data/product-data";
+import { Category } from "@/lib/interfaces/products";
+
 import SearchBarProducts from "@/components/search-bar-products"
 import FeaturedProductList from "@/components/featured-product-list"
 import ProductsList from "@/components/products-list"
@@ -9,18 +13,44 @@ export default async function Products({
   searchParams: Promise<{ [key: string]: string | undefined }>
 }) {
 
-  const { search } = await searchParams
+  // Fetch categories
+  const response = await fetchAllCategories();
+  const results: Category[] = response || [];
+
+  const { search, category } = await searchParams
   const limit = 60;
   const skip = 30;
 
+  // const {category } = await searchParams
+
   return (
+    // Render categories
     <section className="flex justify-around">
+      <div className="px-6 w-2/10">
+        <aside className="p-4 border">
+            <h3 className="pb-4 font-semibold">| Categories</h3>
+            <ul className="pb-2">
+              <CategoryList />
+              {/*               
+              {results.map((category, index) => (
+                  <li key={index}><Link href="#">{category.name}</Link></li>
+              ))}
+              */}
+
+            </ul>
+        </aside>
+      </div>
+
+{/* 
       <div className="px-6 w-2/10">
         <CategoryList />
       </div>
+*/}
+
+
       <div className="w-9/10">
           <SearchBarProducts />
-          {(search && search !== "") ?<ProductsList searchQuery={search}/>: <FeaturedProductList limit={limit} skip={skip} />}
+          {(search && search !== "") ?<ProductsList searchQuery={category, search}/>: <FeaturedProductList limit={limit} skip={skip} />}
       </div>
     </section>
   )
--- components/category-list.tsx
+++ components/category-list.tsx
@@ -1,21 +1,46 @@
-import Link from "next/link";
-import { fetchAllCategories } from "@/lib/data/product-data";
-import { Category } from "@/lib/interfaces/products";
+"use client"
 
-export default async function CategoryList() {
-    const response = await fetchAllCategories();
-    const results: Category[] = response || [];
+import Form from "next/form";
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+
+const typesArray = ["Beauty", "Fragrances", "Furniture", "Groceries", "Home Decoration", "Kitchen Accessories", "Laptops", "Mens Shirts", "Mens Shoes", "Mens Watches", "Mobile Accessories", "Motorcycle", "Skin Care", "Smartphones",
+"Sports Accessories",
+"Sunglasses",
+"Tablets",
+"Tops",
+"Vehicle",
+"Womens Bags",
+"Womens Dresses",
+"Womens Jewellery",
+"Womens Shoes",
+"Womens Watches"]
+
+export default function CategoryList() {
+    const searchParams = useSearchParams();
+    const pathname = usePathname();
+    const { replace } = useRouter();
+    
+    const handleClick = (term: string) => {
+    const params = new URLSearchParams(searchParams);
+    
+    const category = term;
+    params.set('category', category);
+    
+    replace(`${pathname}?${params.toString()}`);
+    }
 
     return (
-        <>
-            <aside className="p-4 border">
-                <h3 className="pb-4 font-semibold">| Categories</h3>
-                <ul className="pb-2">
-                    {results.map((category, index) => (
-                        <li key={index}><Link href="#">{category.name}</Link></li>
-                    ))}
-                </ul>
-            </aside>
-        </>
+        <div className="grid grid-cols-1 justify-start">
+            {typesArray.map((term) => (
+        <button
+            key={term}
+            type="button"
+            className="text-start cursor-pointer hover:text-blue-900"
+            onClick={() => handleClick(term)}
+        >
+            {term}
+        </button>
+            ))}
+        </div>
     )
 }
