9d3f8d3421ef8ed4289d237d6443dfe6a1921acc
Consolider <christian.spector@protonmail.com>
2025-09-18 11:28:49 +0200

    added cursor-pointer to buttons

--- components/product-details/product-actions.tsx
+++ components/product-details/product-actions.tsx
@@ -13,13 +13,13 @@ export default function ProductActions({ title }: ProductHeaderProps) {
       <div className="flex flex-col sm:flex-row items-center gap-4 w-full mb-2">
         <QuantitySelector />
         <button
-          className="bg-black text-center text-sm md:text-base text-white py-2 p-2 rounded-3xl w-full sm:w-4/5 h-10 hover:bg-gray-800 transition-colors"
+          className="bg-black text-center text-sm md:text-base text-white py-2 p-2 rounded-3xl w-full sm:w-4/5 h-10 hover:bg-gray-800 transition-colors cursor-pointer"
           onClick={() => toast.success(`${title} added to cart.`)}
         >
           Add to Cart
         </button>
       </div>
-      <button className="bg-white text-center text-sm md:text-base text-black border border-black py-2 p-2 rounded-3xl w-full mt-2 hover:bg-gray-100 transition-colors">
+      <button className="bg-white text-center text-sm md:text-base text-black border border-black py-2 p-2 rounded-3xl w-full mt-2 hover:bg-gray-100 transition-colors cursor-pointer">
         Buy now
       </button>
     </div>
