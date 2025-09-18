ca28dca79da6c145454c1630bb8a3c35172362a8
Linda <lindakowalik@gmail.com>
2025-09-18 13:22:42 +0200

    removed input field and made buttons functional instead

--- components/product-details/quantity-selector.tsx
+++ components/product-details/quantity-selector.tsx
@@ -1,25 +1,57 @@
+import { useState } from "react";
+
 export default function QuantitySelector() {
-  return (
-    <div className="inline-flex items-center gap-1 border p-2 rounded-3xl w-full sm:w-1/5 h-10 border-gray-300 border-opacity-50 mb-2 sm:mb-0">
-      <button
-        className="h-full w-8 flex items-center justify-center bg-gray-100 hover:bg-gray-200 rounded-md transition-colors"
-        aria-label="Decrease quantity"
-      >
-        -
-      </button>
-      <input
-        type="number"
-        min={1}
-        defaultValue={1}
-        className="w-full h-full text-center rounded focus:outline-none"
-        aria-label="Product quantity"
-      />
-      <button
-        className="h-full w-8 flex items-center justify-center bg-gray-100 hover:bg-gray-200 rounded-md transition-colors"
-        aria-label="Increase quantity"
-      >
-        +
-      </button>
-    </div>
-  );
+    const [quantity, setQuantity] = useState(1)
+
+    const handleDecrease = () => setQuantity(quantity - 1)
+    const handleIncrease = () => setQuantity(quantity + 1)
+
+    const decreaseDisabled = quantity <=1;
+    const increaseDisabled = quantity >= 10;
+    
+
+    return (
+      <div className="inline-flex items-center gap-1 border p-2 rounded-3xl w-full sm:w-1/5 h-10 border-gray-300 border-opacity-50 mb-2 sm:mb-0">
+        
+        {decreaseDisabled ? (
+          <button
+            className="h-full w-8 flex items-center justify-center pointer-events-none opacity-50"
+            aria-label="Decrease quantity"
+            onClick={handleDecrease}
+          >
+            -
+          </button>
+        ) : (
+            <button
+            className="h-full w-8 flex items-center justify-center hover:cursor-pointer"
+            aria-label="Decrease quantity"
+            onClick={handleDecrease}
+          >
+            -
+          </button>
+        )}
+      
+        
+        <span>{quantity}</span>
+
+
+        {increaseDisabled ? (
+          <button
+            className="h-full w-8 flex items-center justify-center pointer-events-none opacity-50"
+            aria-label="Increase quantity"
+            onClick={handleIncrease}
+          >
+          +
+          </button>
+        ) : (
+          <button
+            className="h-full w-8 flex items-center justify-center hover:cursor-pointer"
+            aria-label="Increase quantity"
+            onClick={handleIncrease}
+          >
+            +
+          </button>
+        )}
+      </div>
+    );
 }
