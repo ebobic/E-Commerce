0f617513df986703d3c6f11374c5de3de8cb9ec5
Linda <lindakowalik@gmail.com>
2025-09-29 13:35:41 +0200

    improved layout and responsiveness of ImageGallery and ProductDetailsCard

--- components/product-details/details-card.tsx
+++ components/product-details/details-card.tsx
@@ -18,9 +18,9 @@ export default async function ProductDetailsCard({
   const reviewCount = product.reviews?.length || 0;
 
   return (
-    <article className="flex flex-col md:flex-row w-9/10 lg:5/10 max-w-[1200px] justify-center items-center md:items-start m-auto gap-8 py-2 sm:py-12">
+    <article className="flex flex-col lg:flex-row w-9/10 lg:5/10 max-w-[1200px] justify-center items-center md:items-start m-auto gap-8 py-2 sm:py-12">
         <ImageGallery images={product.images} title={product.title} />
-        <div className=" w-9/10 md:w-1/2 ">
+        <div className="flex m-auto w-9/10 lg:w-1/2 max-w-[800px]">
           <div className="flex flex-col w-full h-full bg-white">
             <ProductHeader title={product.title} />
             <div className="flex flex-col gap-y-2  items-start">
