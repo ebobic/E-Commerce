9c3eb8ae7217ff0807622963fefa3cbc16831253
Consolider <christian.spector@protonmail.com>
2025-09-29 16:51:42 +0200

    refactor contact-form to server component with zod validation

---
+++ app/api/action.ts
@@ -0,0 +1,38 @@
+"use server";
+
+import { z } from "zod";
+
+const contactFormSchema = z.object({
+  name: z.string().trim().min(1, { message: "Name field is required" }),
+  email: z.email({ message: "Invalid email address" }),
+  subject: z.string().trim().min(1, { message: "Subject field required" }),
+  message: z.string().trim().min(1, { message: "Please type a message" }),
+});
+
+// Server Action
+export async function sendEmail(prevState: any, formData: FormData) {
+  // Convert the raw formData into a regular JavaScript object
+  const contactFormData = Object.fromEntries(formData);
+  // Validate contactFormData using the safeParse() method
+  const validatedContactFormData = contactFormSchema.safeParse(contactFormData);
+
+  // If the validation fails, we return an object with an error property, which is an object containing the error message of each form field.
+  if (!validatedContactFormData.success) {
+    const formFieldErrors =
+        validatedContactFormData.error.flatten().fieldErrors;
+    //   z.treeifyError();
+
+    return {
+      errors: {
+        name: formFieldErrors?.name,
+        email: formFieldErrors?.email,
+        subject: formFieldErrors?.subject,
+        message: formFieldErrors?.message,
+      },
+    };
+  }
+
+  return {
+    success: "Your message was sent successfully!",
+  };
+}
---
+++ components/contact-form-client.tsx
@@ -0,0 +1,99 @@
+"use client";
+
+import Form from "next/form";
+import { useActionState } from "react";
+import { useFormStatus } from "react-dom";
+import { sendEmail } from "@/app/api/action"
+
+const initialState = {
+  success: "",
+  errors: {
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  }
+};
+
+function ContactForm() {
+    // const [state, formAction] = useActionState(sendEmail, initialState);
+    const [state, formAction] = useActionState(sendEmail, null);    // Från Roberts exempel
+
+  return (
+    <Form action={formAction} className="grid">
+        <div className="grid gap-2 w-9/10 m-auto sm:w-7/10 sm:flex sm:mb-1">
+          <input
+            type="text"
+            name="name"
+            id="name"
+            className="px-2 rounded-lg bg-blue-100 sm:m-auto sm:w-7/10"
+            aria-label="Contact Us - Name"
+            placeholder="Your name"
+          />
+            {state?.errors?.name && (
+              <p className="text-center text-sm text-red-500">{state.errors.name}</p>
+            )}
+          <input
+            type="email"
+            className="px-2 rounded-lg bg-blue-100 sm:m-auto sm:w-7/10"
+            name="email"
+            id="email"
+            aria-label="Contact Us - E-mail"
+            placeholder="Your e-mail"
+          />
+            {state?.errors?.email && (
+              <p className="text-center text-sm text-red-500">{state.errors.email}</p>
+            )}
+        </div>
+        <input
+          type="text"
+          name="subject"
+          id="subject"
+          className="mt-2 px-2 rounded-lg bg-blue-100 m-auto w-9/10 sm:w-7/10 sm:mt-1"
+          aria-label="Contact Us - Subject"
+          placeholder="Subject"
+        />
+          {state?.errors?.subject && (
+            <p className="text-center text-sm text-red-500">{state.errors.subject}</p>
+          )}
+        <textarea
+          className="mt-2 px-2 rounded-lg bg-blue-100 m-auto w-9/10 sm:w-7/10"
+          rows={5}
+          cols={40}
+          name="message"
+          id="message"
+          aria-label="Contact Us - Message"
+          placeholder="Message"
+        />
+          {state?.errors?.message && (
+            <p className="text-center text-sm text-red-500">{state.errors.message}</p>
+          )}
+        <div className="grid justify-self-center sm:w-7/10 sm:flex sm:items-center sm:justify-between">
+        <SubmitButton />
+          {state?.success && <p className="mt-3 p-2 opacity-70 border text-xs rounded-lg bg-green-100 text-green-600">Message sent successfully!</p>}
+        </div>
+      </Form>
+  );
+}
+
+export default ContactForm;
+
+function SubmitButton() {
+  const { pending } = useFormStatus();
+
+  return (
+    <button
+      type="submit"
+      disabled={pending ? true : false}
+      className="mt-4 py-0.5 px-4 rounded-full text-white bg-blue-900 hover:bg-blue-800 cursor-pointer"
+    >
+      {pending ? (
+        <span>
+          Submitting...
+        </span>
+      ) : (
+        "Send Message"
+      )}
+    </button>
+  );
+}
